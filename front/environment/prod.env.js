8f1aaeabf673e5d22f18d06ea5eb3bd74d1504b7
kengop <kengop@gmail.com>
2020-02-09 11:15:02 +0900

    refs #13 production環境のAPIのホスト名を設定

--- front/environment/prod.env.js
+++ front/environment/prod.env.js
@@ -2,6 +2,5 @@ module.exports = Object.assign({
   ENV: '"production"',
   NODE_ENV: '"production"',
   DEBUG_MODE: false,
-  SEVER_HOST: 'http://localhost:3000'
+  SEVER_HOST: 'https://kuso-app-backend-znfejv3tbq-an.a.run.app'
 });
-  
